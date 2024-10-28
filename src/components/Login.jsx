1aff093d0971eab1a2c75f2581e3309343cbcf0e
Souvik-droid <80137576+Souvik-droid@users.noreply.github.com>
2024-10-29 03:04:10 +0530

    created sign in form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,7 +6,24 @@ const Login = () => {
   return (
     <div className=''>
         <Header />
-        <img className="" src={Login_bg} alt="login background" />
+        <img 
+            className="absolute" 
+            src={Login_bg} alt="login background" 
+        />
+        <form className=' bg-black text-white absolute p-12 w-1/3 my-36 mx-auto right-0 left-0 bg-opacity-80 rounded-md'>
+            <h1 className=' font-bold text-3xl py-4 '>Sign In</h1>
+            <input
+                 type="text" 
+                 placeholder='Email or mobile number ' 
+                 className='p-4 my-3 rounded-md bg-slate-700 bg-opacity-60 w-full'
+            />
+            <input 
+                type="password" 
+                placeholder='Password' 
+                className='p-4 my-3 rounded-md bg-slate-700 bg-opacity-60 w-full'
+            />
+            <button className='bg-red-700  p-2 my-4 rounded-md w-full font-bold'>Sign In</button>
+        </form>
 
     </div>
   )
