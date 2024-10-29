c4e4360ea2c44e2541f944350d464c6b9dec55cb
Souvik-droid <80137576+Souvik-droid@users.noreply.github.com>
2024-10-29 12:24:57 +0530

    updated ui

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -18,36 +18,42 @@ const Login = () => {
             src={Login_bg} alt="login background" 
         />
         <form className=' bg-black text-white absolute p-12 w-1/3 my-32 mx-auto right-0 left-0 bg-opacity-80 rounded-md'>
-            <h1 className=' font-bold text-3xl py-4 '>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
+            <h1 className=' font-bold text-3xl py-2 '>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
             
             {!isSignInForm && (
                 <input
                     type="text" 
                     placeholder='Name' 
-                    className='p-4 my-3 rounded-md bg-slate-700 bg-opacity-60 w-full'
+                    className='p-4 my-2 rounded-md bg-slate-700 bg-opacity-60 w-full'
                 />
             )}
             <input
                  type="text" 
                  placeholder='Email or mobile number ' 
-                 className='p-4 my-3 rounded-md bg-slate-700 bg-opacity-60 w-full'
+                 className='p-4 my-2 rounded-md bg-slate-700 bg-opacity-60 w-full'
             />
             <input 
                 type="password" 
                 placeholder='Password' 
-                className='p-4 my-3 rounded-md bg-slate-700 bg-opacity-60 w-full'
+                className='p-4 my-2 rounded-md bg-slate-700 bg-opacity-60 w-full'
             />
             {!isSignInForm && (
                 <input 
                     type="password" 
                     placeholder='Confirm Password' 
                     
-                    className='p-4 my-3 rounded-md bg-slate-700 bg-opacity-60 w-full'
+                    className='p-4 my-2 rounded-md bg-slate-700 bg-opacity-60 w-full'
                 />
             )}
-            <button className='bg-red-700  p-2 my-4 rounded-md w-full font-bold'>Sign In</button>
-            <h3 className='mx-36 text-lg text-neutral-500'>OR</h3>
-            <p className='py-4 cursor-pointer' onClick={toggleSignInForm}>{isSignInForm ?  "New to netflix? Sign Up Now"  : <span className='font-bold'>Sign In Now</span>}</p>
+            <button className='bg-red-700  p-2 my-3 rounded-md w-full font-bold'>{isSignInForm?"Sign In":"Sign Up"}</button>
+            
+            <h3 className='mx-36 text-lg text-neutral-500 my-1'>OR</h3>
+            
+            <div className='Sign in/up'>
+                <span className='text-neutral-500'>{isSignInForm? "New to Netflix?":"Already have an account?"}</span>
+                <span className='py-2 cursor-pointer font-semibold' onClick={toggleSignInForm}>{isSignInForm ? " Sign Up"  : " Sign In"}</span>
+            </div>
+            
         </form>
 
     </div>
