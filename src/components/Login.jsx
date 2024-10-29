423671a0d2eb0113eb8efb2e16e992798adcfa93
Souvik-droid <80137576+Souvik-droid@users.noreply.github.com>
2024-10-29 08:35:14 +0530

    ui updated

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
             className="absolute" 
             src={Login_bg} alt="login background" 
         />
-        <form className=' bg-black text-white absolute p-12 w-1/3 my-36 mx-auto right-0 left-0 bg-opacity-80 rounded-md'>
+        <form className=' bg-black text-white absolute p-12 w-1/3 my-32 mx-auto right-0 left-0 bg-opacity-80 rounded-md'>
             <h1 className=' font-bold text-3xl py-4 '>Sign In</h1>
             <input
                  type="text" 
@@ -23,6 +23,8 @@ const Login = () => {
                 className='p-4 my-3 rounded-md bg-slate-700 bg-opacity-60 w-full'
             />
             <button className='bg-red-700  p-2 my-4 rounded-md w-full font-bold'>Sign In</button>
+            <h3 className='mx-36 text-lg text-neutral-500'>OR</h3>
+            <p className='py-4'>New to netflix? Sign Up Now</p>
         </form>
 
     </div>
