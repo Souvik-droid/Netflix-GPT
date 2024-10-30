28a7e3e77d9b2cea46fb1c6b24d20d9545bf29e6
Souvik-droid <80137576+Souvik-droid@users.noreply.github.com>
2024-10-30 09:22:04 +0530

    error message displayed

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,23 +1,39 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Login_bg } from '../utils/constant'
 import Header from './Header'
+import { checkValidata } from '../utils/Validate';
 
 const Login = () => {
 
     const [isSignInForm, setIsSignInForm] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
 
+    // Sign in || Sign Up
     const toggleSignInForm = () => {
         setIsSignInForm(!isSignInForm);
     }
+    const email = useRef(null);
+    const password = useRef(null);
+
+    // Check validity
+    const handleButtonClick = () => {
+        console.log(email.current.value);
+        console.log(password.current.value);
+
+        const message = checkValidata(email.current.value,  password.current.value);
+        setErrorMessage(message);
+
+    }
+
 
   return (
-    <div className=''>
+    <div className='bg-gradient-to-b from-black'>
         <Header />
         <img 
             className="absolute" 
             src={Login_bg} alt="login background" 
         />
-        <form className=' bg-black text-white absolute p-12 w-1/3 my-32 mx-auto right-0 left-0 bg-opacity-80 rounded-md'>
+        <form onSubmit={(e) => e.preventDefault()} className=' bg-black text-white absolute p-12 w-1/3 my-32 mx-auto right-0 left-0 bg-opacity-80 rounded-md'>
             <h1 className=' font-bold text-3xl py-2 '>{isSignInForm ? "Sign In" : "Sign Up"}</h1>
             
             {!isSignInForm && (
@@ -28,12 +44,14 @@ const Login = () => {
                 />
             )}
             <input
-                 type="text" 
+                 type="text"
+                 ref={email} 
                  placeholder='Email or mobile number ' 
                  className='p-4 my-2 rounded-md bg-slate-700 bg-opacity-60 w-full'
             />
             <input 
                 type="password" 
+                ref={password}
                 placeholder='Password' 
                 className='p-4 my-2 rounded-md bg-slate-700 bg-opacity-60 w-full'
             />
@@ -45,7 +63,8 @@ const Login = () => {
                     className='p-4 my-2 rounded-md bg-slate-700 bg-opacity-60 w-full'
                 />
             )}
-            <button className='bg-red-700  p-2 my-3 rounded-md w-full font-bold'>{isSignInForm?"Sign In":"Sign Up"}</button>
+            <p className='text-red-600'>{errorMessage}</p>
+            <button className='bg-red-700  p-2 my-3 rounded-md w-full font-bold' onClick={handleButtonClick}>{isSignInForm?"Sign In":"Sign Up"}</button>
             
             <h3 className='mx-36 text-lg text-neutral-500 my-1'>OR</h3>
             
